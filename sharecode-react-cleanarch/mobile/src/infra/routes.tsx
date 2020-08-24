bb017e764deca93282a9969ff33e1cfc456adacd
Alexandre Martins <alexandre.martins@opah.com.br>
2020-08-23 23:04:34 -0300

    Estruturação code sharring + clean arch + mobile google maps

---
+++ sharecode-react-cleanarch/mobile/src/infra/routes.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { NativeRouter, Route, Switch } from 'react-router-native';
+import Main from "../presentation/pages/Main";
+import store from './store';
+
+const Routes = () => (
+    <Provider store={store}>
+        <NativeRouter>
+            <Switch>
+                <Route exact path="/" component={Main} />
+                <Route path="*" component={() => <View>Page not found</View>} />
+            </Switch>
+        </NativeRouter>
+    </Provider>
+);
+
+export default Routes;
---
+++ sharecode-react-cleanarch/mobile/src/presentation/pages/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import Maps from "../components/Maps";
+
+// import { Container } from './styles';
+
+const Main: React.FC = () => (
+  <View>
+      <Maps />
+  </View>
+)
+
+export default Main;
